84cf5e5d0e536e80ac0cde3e9f0372e83074b73c
ramin29 <130083945+ramin29@users.noreply.github.com>
2023-04-08 01:24:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,13 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+  
+    qrr = np.ndarray(1000)  # range like только в numpy
+    lst = list(range(1000))
+    
+    t = 10
+    a = qrr/t
+
+    a_m = np.mean(a)
+    o = 1/a_m
     return x.mean() # Ваш ответ
